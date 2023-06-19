1dafee8d81554e9750dd14f706a1c74cfdd6ed28
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-19 20:26:41 -0300

    new proyect, bomberos santa catalina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    ContactoComponent,
+    PerfilComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
