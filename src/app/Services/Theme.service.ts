1b7fd67d7380184f8f4e2bdc085f218a709a3cff
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-20 22:06:07 -0300

    add icons boostraps and more styles global

--- src/app/Services/Theme.service.ts
+++ src/app/Services/Theme.service.ts
@@ -15,5 +15,3 @@ export class ThemeService {
     console.log(newTheme)
   }
 }
-
-//
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './Services/Theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-bomberos';
+  constructor(private localeTheme: ThemeService) { }
+
+  
 }
