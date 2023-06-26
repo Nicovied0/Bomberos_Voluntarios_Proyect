9f09f3792a5f172938a92a7632d8fec702670e6a
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-26 19:52:29 -0300

    add vehicles models to the app

---
+++ api/src/models/Vehicles.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const VehiclesSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  mileage: {
+    type: Number,
+    default: 0,
+  },
+  tankSize: {
+    type: Number,
+    default: 0,
+  },
+  amountOfFuel: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Vehicles = mongoose.model("Vehicle", VehiclesSchema);
+
+module.exports = Vehicles;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
+// Obtener todos los todos
+// router.get('/todos', async (req, res) => {
+//   try {
+//     const todos = await Todo.find();
+//     res.json(todos);
+//   } catch (error) {
+//     console.error('Error al obtener los todos', error);
+//     res.status(500).json({ error: 'Error al obtener los todos' });
+//   }
+// });
 
+// // Crear un nuevo todo
+// router.post('/todos', async (req, res) => {
+//   try {
+//     const { title } = req.body;
+//     const todo = new Todo({
+//       title,
+//     });
+//     await todo.save();
+//     res.json(todo);
+//   } catch (error) {
+//     console.error('Error al crear el todo', error);
+//     res.status(500).json({ error: 'Error al crear el todo' });
+//   }
+// });
 
 module.exports = router;
