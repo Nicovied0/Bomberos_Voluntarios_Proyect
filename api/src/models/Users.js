0980bb524afceb9f802fd2e7bf848a330ddf1420
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-26 22:15:39 -0300

    add new model

---
+++ api/src/models/Users.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imagen: {
+    type:String,
+    required: true
+  },
+ 
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Users = mongoose.model("User", UsersSchema);
+
+module.exports = Users;
