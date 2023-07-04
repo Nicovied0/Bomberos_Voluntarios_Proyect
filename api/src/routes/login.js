bd6c908778ebe2b54e1226ccaa77d7a1599e188d
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-03 23:56:31 -0300

    add rotue get login

--- api/src/routes/login.js
+++ api/src/routes/login.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  res.json("Ruta login")
+});
+
 router.post("/", async (req, res) => {});
 
 module.exports = router;
