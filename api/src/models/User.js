044f5723ea3756cf0ba649635df852ecd2b535da
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-03 21:07:14 -0300

    add more spects in schema  user

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -7,11 +7,17 @@ const UserSchema = new mongoose.Schema({
   },
   imagen: {
     type: String,
-    required: true,
+    default:
+      "https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png",
   },
   email: {
     type: String,
     required: true,
+  },
+  pass: {
+    type: String,
+    required: true,
+    default: "userpass",
   },
 
   createdAt: {
