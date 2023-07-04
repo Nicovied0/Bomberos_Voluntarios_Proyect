eb3959f4a737d5b0fb98639625189fa94c63fb47
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-04 14:25:37 -0300

    add inputs in registerform component

--- client/src/app/register-form/register-form.component.ts
+++ client/src/app/register-form/register-form.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent {
-
+  formData = {
+    name: '',
+    email: '',
+    password: ''
+  };
 }
