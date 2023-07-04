88127b5e123806c26dca8021a2636a6caacee98c
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-03 23:54:18 -0300

    add new route login in back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const vehiclesRoutes = require("./vehicles");
+const loginRoutes = require("./login");
 const uploadData = require("./uploadData");
 
 
-
-
 router.use("/vehicles", vehiclesRoutes);
+router.use("/login", loginRoutes);
 router.use("/upload", uploadData);
 
 
-
 module.exports = router;
-
