59e633ea09f8a1ed4a705103d94d9848b46c3440
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-04 14:34:27 -0300

    add submit register in registerform component

--- client/src/app/register-form/register-form.component.ts
+++ client/src/app/register-form/register-form.component.ts
@@ -13,11 +13,21 @@ export class RegisterFormComponent {
     email: '',
     password: ''
   };
-  
+
   constructor(private authService: AuthService) { }
 
 
   onSubmit() {
-
+    this.authService.register(this.formData.name, this.formData.email, this.formData.password)
+      .subscribe(
+        response => {
+          console.log('Registro exitoso');
+          // Aquí puedes redirigir a otra página o mostrar un mensaje de éxito
+        },
+        error => {
+          console.log('Error en el registro', error);
+          // Aquí puedes mostrar un mensaje de error o manejar el error de otra forma
+        }
+      );
   }
 }
