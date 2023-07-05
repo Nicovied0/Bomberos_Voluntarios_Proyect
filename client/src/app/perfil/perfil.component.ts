0ac7b04d058cc31da8e5632a0f708dc879b7ec67
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-04 22:12:53 -0300

    get token to coprobate if session started

--- client/src/app/perfil/perfil.component.ts
+++ client/src/app/perfil/perfil.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
+  private loged = false
 
+  ngOnInit() {
+
+    this.getUser()
+  }
+  getUser() {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      console.log('Token encontrado:', token);
+      this.loged = true
+    } else {
+      console.log('No se encontró un token');
+      this.loged = true
+    }
+
+  }
 }
