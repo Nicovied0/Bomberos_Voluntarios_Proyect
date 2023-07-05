c6cee6bc691d9e85cbe6cfe3cb31ad29fb7f1265
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-04 22:49:52 -0300

    add route get profile with token in back

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -4,10 +4,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-router.get("/", async (req, res) => {
-  res.json("Ruta login");
-});
-
 // Ruta para el registro de usuarios
 router.post("/register", (req, res) => {
   const { name, email, password } = req.body;
@@ -71,6 +67,45 @@ router.post("/login", (req, res) => {
     .catch((error) => {
       res.status(500).json({ message: "Error en el servidor" });
     });
+});
+
+router.get("/profile", (req, res) => {
+  // Verificar el token de acceso
+  const token = req.headers.token;
+  if (!token) {
+    return res.status(401).json({ message: "Token no proporcionado" });
+  }
+
+  // Verificar y decodificar el token
+  jwt.verify(token, "secreto", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token inválido" });
+    }
+
+    // Obtener el ID del usuario del token decodificado
+    const userId = decoded.userId;
+
+    // Buscar el usuario por su ID en la base de datos
+    User.findById(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ message: "Usuario no encontrado" });
+        }
+
+        // Aquí puedes obtener los datos del perfil que deseas guardar en el localStorage
+        const userProfile = {
+          name: user.name,
+          email: user.email,
+          // Agrega cualquier otra información adicional del perfil que necesites
+        };
+
+        // Guardar los datos del perfil en el localStorage
+        res.status(200).json({ profile: userProfile });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Error en el servidor" });
+      });
+  });
 });
 
 module.exports = router;
