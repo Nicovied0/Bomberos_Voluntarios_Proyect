618667f96b0e8e007b7c452b653ab6a86ef8ccdc
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-06 00:10:20 -0300

    add edit profile component, succefull

--- client/src/app/edit-profile/edit-profile.component.ts
+++ client/src/app/edit-profile/edit-profile.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileService } from '../Services/Profile.service';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent {
-  constructor(private router: Router) { }
+
+export class EditProfileComponent implements OnInit {
+  public profileData = {
+    name: '',
+    email: '',
+    imagen: ''
+  }
+
+  constructor(private router: Router, private profileService: ProfileService) { }
+
+  ngOnInit() {
+    this.getProfileData();
+  }
+
+  getProfileData() {
+    const profile = localStorage.getItem('profile');
+    if (profile) {
+      this.profileData = JSON.parse(profile);
+    }
+  }
+
+  submitForm() {
+    this.profileService.updateProfileData(this.profileData).subscribe(
+      (response: any) => {
+        if (response.profile) {
+          const updatedProfile = response.profile;
+          localStorage.setItem('profile', JSON.stringify(updatedProfile));
+          this.router.navigate(['/perfil']);
+        } else {
+          console.log('No se pudo actualizar el perfil');
+        }
+      },
+      error => {
+        console.log('Error al actualizar el perfil', error);
+      }
+    );
+  }
 }
