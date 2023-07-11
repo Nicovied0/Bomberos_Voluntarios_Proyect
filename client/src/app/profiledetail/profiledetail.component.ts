0f42f057f6f6d3075019b7076afd8ad63aa07036
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-11 17:59:09 -0300

    add logic in componente profile detail

--- client/src/app/profiledetail/profiledetail.component.ts
+++ client/src/app/profiledetail/profiledetail.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from '../Services/Profile.service';
 
 @Component({
   selector: 'app-profiledetail',
   templateUrl: './profiledetail.component.html',
   styleUrls: ['./profiledetail.component.css']
 })
-export class ProfiledetailComponent {
 
+export class ProfiledetailComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private profileService: ProfileService
+  ) { }
+
+  public profileData = {
+    name: '',
+    email: '',
+    imagen: '',
+    number: ''
+  };
+
+  ngOnInit() {
+    this.getProfileData();
+  }
+
+  getProfileData() {
+    const profile = localStorage.getItem('profile');
+    if (profile) {
+      this.profileData = JSON.parse(profile);
+    }
+  }
+  
+  goProfile() {
+    this.router.navigate(['/perfil'])
+  }
 }
