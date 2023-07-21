da214f4ed4b6d2d47bfeb0ee6e5a71b7031fd8f8
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-20 23:29:10 -0300

    sadjas0a

--- client/src/app/admin-post/admin-post.component.ts
+++ client/src/app/admin-post/admin-post.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../Services/Post.service';
+import { Publicacion } from '../Services/publicacion.inteface';
+import { DomSanitizer,SafeHtml  } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-post',
@@ -8,23 +10,45 @@ import { Router } from '@angular/router';
 })
 export class AdminPostComponent implements OnInit {
 
-  constructor(private router: Router, private postService: PostService) { }
-  posts: any[] = [];
+  constructor(private router: Router ,private postService: PostService, private sanitizer: DomSanitizer) { }
+  publicaciones: Publicacion[] = [];
+  iframeLink300 = "";
+  iframeLink400 = "";
+  iframeLink500 = "";
 
   ngOnInit() {
-    this.getPosts();
+    this.getPublicaciones();
   }
 
-  getPosts() {
+  getPublicaciones() {
     this.postService.obtenerPublicaciones().subscribe(
-      (postResponse: any[]) => {
-        this.posts = postResponse;
-        console.log(this.posts);
+      (publicaciones: Publicacion[]) => {
+        this.publicaciones = publicaciones;
+        if (publicaciones.length > 0) {
+          // Asignar los valores según los objetos en el arreglo publicaciones
+          this.iframeLink300 = this.removeBackslashes(publicaciones[0]?.iframeLink300 || '');
+          this.iframeLink400 = this.removeBackslashes(publicaciones[0]?.iframeLink400 || '');
+          this.iframeLink500 = this.removeBackslashes(publicaciones[0]?.iframeLink500 || '');
+        } else {
+          // Si no hay publicaciones, establecer los enlaces en vacío
+          this.iframeLink300 = '';
+          this.iframeLink400 = '';
+          this.iframeLink500 = '';
+        }
+        console.log(publicaciones);
       },
       (error) => {
         console.error('Error al obtener las publicaciones:', error);
       }
     );
+  }
+
+  removeBackslashes(text: string): string {
+    return text.replace(/\\/g, '');
+  }
+
+  getSafeIframeUrl(iframeLink: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(iframeLink);
   }
 
   goNew(){
