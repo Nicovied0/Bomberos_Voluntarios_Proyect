7567f04ebcd20d2af85af4d5bc9666f19c0a9ae6
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-21 17:27:50 -0300

    Kaksd

--- client/src/app/admin-movil/admin-movil.component.ts
+++ client/src/app/admin-movil/admin-movil.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VehicleService } from '../Services/Vehicles.service';
+import Swal from 'sweetalert2'; // Importa sweetalert2
 
 @Component({
   selector: 'app-admin-movil',
   templateUrl: './admin-movil.component.html',
   styleUrls: ['./admin-movil.component.css']
 })
-export class AdminMovilComponent {
+export class AdminMovilComponent implements OnInit {
+  constructor(private router: Router, private vehicleService: VehicleService) { }
+  vehicles: any[] = [];
+
+  ngOnInit() {
+    this.getVehicles();
+  }
+
+  async getVehicles() {
+    try {
+      const usersResponse = await this.vehicleService.getVehicles();
+      this.vehicles = usersResponse as any[];
+      console.log(this.vehicles);
+    } catch (error) {
+      console.error('Error al obtener los usuarios:', error);
+    }
+  }
 
 }
