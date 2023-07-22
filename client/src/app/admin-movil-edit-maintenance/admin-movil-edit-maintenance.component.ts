e994d28601f20fc558ab076a6bc16cd9c7a9752b
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-21 21:28:47 -0300

    Ppskdapoaps

--- client/src/app/admin-movil-edit-maintenance/admin-movil-edit-maintenance.component.ts
+++ client/src/app/admin-movil-edit-maintenance/admin-movil-edit-maintenance.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VehicleService } from '../Services/Vehicles.service';
 @Component({
   selector: 'app-admin-movil-edit-maintenance',
   templateUrl: './admin-movil-edit-maintenance.component.html',
   styleUrls: ['./admin-movil-edit-maintenance.component.css']
 })
-export class AdminMovilEditMaintenanceComponent {
+export class AdminMovilEditMaintenanceComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private vehicleService: VehicleService
+  ) { }
+  movilId: string | null = null;
+  movil: any | null;
+
+  ngOnInit() {
+    // Obtener el ID del usuario desde el parámetro de la ruta
+    this.movilId = this.route.snapshot.paramMap.get('id');
+    // Obtener la información del usuario específico
+    this.getVehicleDetails();
+
+  }
+
+  async getVehicleDetails() {
+    try {
+      const userResponse = await this.vehicleService.getVehiclesById(this.movilId!);
+      this.movil = userResponse as any;
+      console.log(this.movil)
+    } catch (error) {
+      console.error('Error al obtener los detalles del usuario:', error);
+    }
+  }
 
 }
