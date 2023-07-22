885145b322536f008bace017eda5ec5ee154c6a3
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-22 14:41:18 -0300

    akdps

--- client/src/app/vehicle-information/vehicle-information.component.ts
+++ client/src/app/vehicle-information/vehicle-information.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VehicleService } from '../Services/Vehicles.service';
 
 @Component({
   selector: 'app-vehicle-information',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class VehicleInformationComponent {
 
+  constructor(private route: ActivatedRoute, private vehicleService: VehicleService, private router: Router) { }
+  @Input() vehicle: any;
+  public vehicleId: any;
+
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const vehicleId = params.get('id');
+      if (vehicleId) {
+        this.getVehicle(vehicleId);
+      }
+    });
+  }
+
+  getVehicle(id: any) {
+    this.vehicleService.getVehiclesById(id)
+      .then(vehicle => {
+        console.log(vehicle);
+        this.vehicleId = vehicle;
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 }
