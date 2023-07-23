9aff6b2b8d89fb32897f0b538b09b13bdb5a2b5b
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-23 10:14:45 -0300

    JOjd

--- client/src/app/carrusel/carrusel.component.ts
+++ client/src/app/carrusel/carrusel.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CarruselComponent {
   images = [
-    { url: 'https://i.imgur.com/rKlPmiK.png', title: 'Bomberos Santa Catalina', description: '' },
-    { url: 'https://i.imgur.com/8WTw18h.jpeg', title: 'Correo Para Aspirantes', description: 'email.ejemplo@gmail.com' },
-    { url: 'https://i.imgur.com/dWnVck5.jpeg', title: 'Nuestras redes sociales', description: `Si querés enterarte de las últimas noticias al instante, puedes seguirnos o darle 'Me Gusta' a nuestra página de  Facebook . También, si quieres ver más contenido fotográfico, nos puedes buscar en   Instagram` },
+    { url: 'https://i.imgur.com/tDlggdI.png', title: 'Bomberos Santa Catalina', description: '' },
+    { url: 'https://i.imgur.com/eHcUzN6.jpeg', title: 'Correo Para Aspirantes', description: 'email.ejemplo@gmail.com' },
+    { url: 'https://i.imgur.com/HyQChy5.jpeg', title: 'Nuestras redes sociales', description: `Si querés enterarte de las últimas noticias al instante, puedes seguirnos o darle 'Me Gusta' a nuestra página de  Facebook . También, si quieres ver más contenido fotográfico, nos puedes buscar en   Instagram` },
   ];
 }
