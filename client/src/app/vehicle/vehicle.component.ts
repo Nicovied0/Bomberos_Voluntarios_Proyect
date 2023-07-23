af1bd02cd4b4bcb2ad5953eac982005d01b60920
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-07-23 11:30:05 -0300

    add verifycation to user roles

--- client/src/app/vehicle/vehicle.component.ts
+++ client/src/app/vehicle/vehicle.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VehicleService } from '../Services/Vehicles.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../Services/Auth.service';
 
 
 @Component({
@@ -12,8 +13,9 @@ import { Router } from '@angular/router';
 
 export class VehicleComponent {
   @Input() vehicle: any;
-  constructor(private route: ActivatedRoute, private vehicleService: VehicleService, private router: Router) { }
+  constructor(private authService: AuthService, private route: ActivatedRoute, private vehicleService: VehicleService, private router: Router) { }
   public vehicleId: any;
+  public userLoged: boolean = false
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -22,6 +24,7 @@ export class VehicleComponent {
         this.getVehicle(vehicleId);
       }
     });
+    this.userVerify()
   }
 
   getVehicle(id: any) {
@@ -42,12 +45,16 @@ export class VehicleComponent {
     const year = date.getFullYear();
     return `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
   }
-  
-  goUpdate(id:any){
-    this.router.navigate(['/panelAdmin/Vehicles/maintenance',id])
+
+  goUpdate(id: any) {
+    this.router.navigate(['/panelAdmin/Vehicles/maintenance', id])
   }
-  goInfo(id:any){
-    this.router.navigate(['moviles/Adicional',id])
+  goInfo(id: any) {
+    this.router.navigate(['moviles/Adicional', id])
+  }
+
+  userVerify() {
+    this.userLoged = this.authService.adminUser()
   }
 
 }
